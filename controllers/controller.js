0a7ab928a210dc6435a28ccbc9b2cbbe3e7ebfac
Manish P <invincible.manish1417@gmail.com>
2021-12-24 11:43:07 +0530

    added frontend dashboard

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,14 +1,3 @@
 
 
-const searchData = async (req, res) => {
-    try {
-
-        const data = await MOne.find().lean()
-        res.render("dashboard", { title: "Dashboard", users: data })
-
-    }catch(err) {
-        console.log(err)
-    }
-
-}
-module.exports = searchData
+// will use to scale this application
--- routes/route.js
+++ routes/route.js
@@ -2,16 +2,84 @@ const router= require('express').Router();
 const controller = require('../controllers/controller'); 
 const YoutubeDataModel = require('../models/YoutubeData');
 
-router.get('/', async(req,res) => {
-    try{
-        const data = await YoutubeDataModel.find()
-        res.render("dashboard", { title: "Dashboard", users: data })
 
+router.get('/api', async(req,res) => {
+    try{
+        const resultsPerPage = 5; //a
+        const numOfResults =50;
+        const { page=1, limit=5} = req.query; //a
+        const data = await YoutubeDataModel.find().sort({"publishedAt": -1})
+            .limit(limit)
+            .skip((page-1)*limit);
+        //res.render("dashboard", { title: "Dashboard", users: data })
+        //a
+        const numberOfPages = Math.ceil(numOfResults / resultsPerPage);
+        if(page > numberOfPages){
+            res.redirect('/?page='+encodeURIComponent(numberOfPages));
+        }else if(page < 1){
+            res.redirect('/?page='+encodeURIComponent('1'));
+        }
+
+        let iterator = (page - 5) < 1 ? 1 : page - 5;
+        let endingLink = (iterator + 9) <= numberOfPages ? (iterator + 9) : page + (numberOfPages - page);
+        if(endingLink < (page + 4)){
+            iterator -= (page + 4) - numberOfPages;
+        }
+        res.send({page, limit, data: data})
+//a
     }catch (err) {
         console.log(err)
     }
 })
 
-//router.get('/search', controller.searchData)
+router.get('/search', async(req,res) => {
+    try {
+        const title = req.query.videoTitle
+        const description = req.query.videoDescription
+        if(!title) {
+            const data = await YoutubeDataModel.find({videoDescription: description}).lean()
+            res.send(data)
+        }
+        if(!description) { 
+            const data = await YoutubeDataModel.find({videoTitle: title}).lean()
+            res.send(data)
+        }
+
+    }catch(err) {
+        console.log(err)
+    }
+})
+
+router.get('/', async(req,res) => {
+    try{
+        const resultsPerPage = 5; //a
+        const numOfResults =50;
+        const { page=1, limit=5} = req.query; //a
+        const data = await YoutubeDataModel.find().sort({"publishedAt": -1})
+            .limit(limit)
+            .skip((page-1)*limit);
+        //res.render("dashboard", { title: "Dashboard", users: data })
+        //a
+        const numberOfPages = Math.ceil(numOfResults / resultsPerPage);
+        if(page > numberOfPages){
+            res.redirect('/?page='+encodeURIComponent(numberOfPages));
+        }else if(page < 1){
+            res.redirect('/?page='+encodeURIComponent('1'));
+        }
+
+        let iterator = (page - 5) < 1 ? 1 : page - 5;
+        let endingLink = (iterator + 9) <= numberOfPages ? (iterator + 9) : page + (numberOfPages - page);
+        if(endingLink < (page + 4)){
+            iterator -= (page + 4) - numberOfPages;
+        }
+        //res.send(data).redirect("index")
+        res.render("dashboard", { title: "Dashboard", users: data , page: page, iterator: iterator, endingLink: endingLink, numberOfPages: numberOfPages})
+//a
+    }catch (err) {
+        console.log(err)
+    }
+})
+
+
 
 module.exports = router;
